a8301d97f48266baefc03d12c975b2762247e77a
Yixing Lao <yixing.lao@intel.com>
2018-04-18 15:59:51 -0700

    update parse compile bench

--- parse_compile_bench.py
+++ parse_compile_bench.py
@@ -1,23 +1,25 @@
 import re
+import csv
 
 input_file = "build/compile_bench_out.txt"
-output_file = "build/compile_bench_out.csv"
+writer = csv.writer(open("build/compile_bench_out.csv", "w"))
 
-with open(output_file, 'w') as fo:
-    with open(input_file, 'r') as fi:
-        lines = fi.readlines()
-        lines = [line.strip() for line in lines]
-        lines = "\n".join(lines)
+writer.writerow(["frontend_opt_levels", "backend_opt_levels", "compile_times", "per_iteration_times"])
 
-        frontend_opt_levels = re.findall(r'FRONTEND_OPT_LEVEL=(.) ', lines)
-        print(frontend_opt_levels)
-        backend_opt_levels = re.findall(r'BACKEND_OPT_LEVEL=(.) ', lines)
-        print(backend_opt_levels)
-        compile_times = re.findall(r'compile time: (.+?)ms', lines)
-        print(compile_times)
-        per_iteration_times = re.findall(r'\n(.+?)ms per iteration', lines)
-        print(per_iteration_times)
+with open(input_file, 'r') as fi:
+    lines = fi.readlines()
+    lines = [line.strip() for line in lines]
+    lines = "\n".join(lines)
 
-        for f, b, c, i in zip(frontend_opt_levels, backend_opt_levels,
-                              compile_times, per_iteration_times):
-            print(", ".join([f, b, c, i]), file=fo)
+    frontend_opt_levels = re.findall(r'FRONTEND_OPT_LEVEL=(.) ', lines)
+    print(frontend_opt_levels)
+    backend_opt_levels = re.findall(r'BACKEND_OPT_LEVEL=(.) ', lines)
+    print(backend_opt_levels)
+    compile_times = re.findall(r'compile time: (.+?)ms', lines)
+    print(compile_times)
+    per_iteration_times = re.findall(r'\n(.+?)ms per iteration', lines)
+    print(per_iteration_times)
+
+    for f, b, c, i in zip(frontend_opt_levels, backend_opt_levels,
+                            compile_times, per_iteration_times):
+        writer.writerow([f, b, c, i])
