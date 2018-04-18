4a524df556edb3feb73d9559d1bd0f12fe77d45f
Yixing Lao <yixing.lao@intel.com>
2018-04-18 11:19:41 -0700

    repeat in outer looop

--- run_compile_bench.sh
+++ run_compile_bench.sh
@@ -6,7 +6,7 @@
 # Parameters
 NBENCH="/home/yixing/repo/ngraph/build/src/tools/nbench/nbench"
 BACKEND=CPU
-ITERATION=10
+ITERATION=20
 
 # MODEL="/home/yixing/repo/ngraph-models/models/tensorflow/resnet50_I1k/tf_function_cluster_8.v1836.json"
 # MODEL="/home/yixing/repo/ngraph-models/models/tensorflow/mnist_mlp/tf_function_cluster_4.v61.json"
@@ -16,17 +16,19 @@ MODEL="/home/yixing/repo/ngraph/test/models/mxnet/LSTM_backward.json"
 make -j -C /home/yixing/repo/ngraph/build
 
 # Run
-for FRONTEND_OPT_LEVEL in 0 1 2 3
-do
-    for BACKEND_OPT_LEVEL in 0 1 2 3
+for REPEAT in 0 1 2 3 4 5 6 7 8 9
     do
-        for REPEAT in 0 1 2 3 4 5 6 7 8 9
+    for FRONTEND_OPT_LEVEL in 0 1 2 3
+    do
+        for BACKEND_OPT_LEVEL in 0 1 2 3
         do
-            echo ""
-            echo "###############################"
-            echo "[FRONTEND_OPT_LEVEL=${FRONTEND_OPT_LEVEL} BACKEND_OPT_LEVEL=${BACKEND_OPT_LEVEL} REPEAT=${REPEAT}]"
-            FRONTEND_OPT_LEVEL=${FRONTEND_OPT_LEVEL} BACKEND_OPT_LEVEL=${BACKEND_OPT_LEVEL} \
-                ${NBENCH} -f ${MODEL} -b ${BACKEND} -i ${ITERATION}
+
+                echo ""
+                echo "###############################"
+                echo "[FRONTEND_OPT_LEVEL=${FRONTEND_OPT_LEVEL} BACKEND_OPT_LEVEL=${BACKEND_OPT_LEVEL} REPEAT=${REPEAT}]"
+                FRONTEND_OPT_LEVEL=${FRONTEND_OPT_LEVEL} BACKEND_OPT_LEVEL=${BACKEND_OPT_LEVEL} \
+                    ${NBENCH} -f ${MODEL} -b ${BACKEND} -i ${ITERATION}
+
         done
     done
 done
