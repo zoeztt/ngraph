0050fddf98c6736c773da1333d1216f268e71972
Robert Kimball <robert.kimball@intel.com>
2018-11-28 10:12:34 -0800

    wip

--- src/tools/nbench/benchmark.cpp
+++ src/tools/nbench/benchmark.cpp
@@ -39,6 +39,124 @@ void set_denormals_flush_to_zero()
 #endif
 }
 
+template <typename T>
+void init_int_tv(shared_ptr<runtime::Tensor> tv, T min, T max)
+{
+    size_t size = tv->get_element_count();
+    uniform_int_distribution<T> dist(min, max);
+    vector<T> vec(size);
+    for (T& element : vec)
+    {
+        element = dist(s_random_engine);
+    }
+    tv->write(vec.data(), 0, vec.size() * sizeof(T));
+}
+
+template <>
+void init_int_tv<char>(shared_ptr<runtime::Tensor> tv, char min, char max)
+{
+    size_t size = tv->get_element_count();
+    uniform_int_distribution<int16_t> dist(static_cast<short>(min), static_cast<short>(max));
+    vector<char> vec(size);
+    for (char& element : vec)
+    {
+        element = static_cast<char>(dist(s_random_engine));
+    }
+    tv->write(vec.data(), 0, vec.size() * sizeof(char));
+}
+
+template <>
+void init_int_tv<int8_t>(shared_ptr<runtime::Tensor> tv, int8_t min, int8_t max)
+{
+    size_t size = tv->get_element_count();
+    uniform_int_distribution<int16_t> dist(static_cast<short>(min), static_cast<short>(max));
+    vector<int8_t> vec(size);
+    for (int8_t& element : vec)
+    {
+        element = static_cast<int8_t>(dist(s_random_engine));
+    }
+    tv->write(vec.data(), 0, vec.size() * sizeof(int8_t));
+}
+
+template <>
+void init_int_tv<uint8_t>(shared_ptr<runtime::Tensor> tv, uint8_t min, uint8_t max)
+{
+    size_t size = tv->get_element_count();
+    uniform_int_distribution<int16_t> dist(static_cast<short>(min), static_cast<short>(max));
+    vector<uint8_t> vec(size);
+    for (uint8_t& element : vec)
+    {
+        element = static_cast<uint8_t>(dist(s_random_engine));
+    }
+    tv->write(vec.data(), 0, vec.size() * sizeof(uint8_t));
+}
+
+template <typename T>
+void init_real_tv(shared_ptr<runtime::Tensor> tv, T min, T max)
+{
+    size_t size = tv->get_element_count();
+    uniform_real_distribution<T> dist(min, max);
+    vector<T> vec(size);
+    for (T& element : vec)
+    {
+        element = dist(s_random_engine);
+    }
+    tv->write(vec.data(), 0, vec.size() * sizeof(T));
+}
+
+static void random_init(shared_ptr<runtime::Tensor> tv)
+{
+    element::Type et = tv->get_element_type();
+    if (et == element::boolean)
+    {
+        init_int_tv<char>(tv, 0, 1);
+    }
+    else if (et == element::f32)
+    {
+        init_real_tv<float>(tv, -1, 1);
+    }
+    else if (et == element::f64)
+    {
+        init_real_tv<double>(tv, -1, 1);
+    }
+    else if (et == element::i8)
+    {
+        init_int_tv<int8_t>(tv, -1, 1);
+    }
+    else if (et == element::i16)
+    {
+        init_int_tv<int16_t>(tv, -1, 1);
+    }
+    else if (et == element::i32)
+    {
+        init_int_tv<int32_t>(tv, 0, 1);
+    }
+    else if (et == element::i64)
+    {
+        init_int_tv<int64_t>(tv, -1, 1);
+    }
+    else if (et == element::u8)
+    {
+        init_int_tv<uint8_t>(tv, 0, 1);
+    }
+    else if (et == element::u16)
+    {
+        init_int_tv<uint16_t>(tv, 0, 1);
+    }
+    else if (et == element::u32)
+    {
+        init_int_tv<uint32_t>(tv, 0, 1);
+    }
+    else if (et == element::u64)
+    {
+        init_int_tv<uint64_t>(tv, 0, 1);
+    }
+    else
+    {
+        throw runtime_error("unsupported type");
+    }
+}
+
 vector<runtime::PerformanceCounter> run_benchmark(shared_ptr<Function> f,
                                                   const string& backend_name,
                                                   size_t iterations,
